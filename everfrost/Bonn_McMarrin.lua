f29d92657ba984d617d63fd60019d1114013cf22
proxeeus <proxeeus@gmail.com>
2017-04-29 11:15:20 +0200

    bit of cleanup for everfrost + a bit of flavor text as well

---
+++ everfrost/Bonn_McMarrin.lua
@@ -0,0 +1,7 @@
+function event_combat(e)
+	e.self:Say("Prepare to bleed!!");
+end
+
+function event_death_complete(e)
+	e.self:Say("The Wolves o' the North shall track ye down and avenge me death.");
+end
