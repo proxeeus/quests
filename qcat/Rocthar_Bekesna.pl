46a086c2cfa84607bcc69a222b92c003b827dd27
proxeeus <proxeeus@gmail.com>
2017-07-15 14:48:17 +0200

    added missing dialogue to Rocthar_Bekesna.pl

--- qcat/Rocthar_Bekesna.pl
+++ qcat/Rocthar_Bekesna.pl
@@ -6,16 +6,26 @@ sub EVENT_SPAWN {
 
 sub EVENT_ENTER {
   if(plugin::check_hasitem($client, 18853)) { 
-		$client->Message(15,"A dark whisper of a voice seems to echo in your mind. 'Welcome young warrior of Bertoxxulous. I am Rocthar Bekesna, you're new Guild Master. Read the note in your inventory and then hand it to me to begin your training.'");
+		$client->Message(15,"A dark whisper of a voice seems to echo in your mind. 'Welcome young warrior of Bertoxxulous. I am Rocthar Bekesna, your Guild Master. Read the note in your inventory and then hand it to me to begin your training.'");
   }
 }
 
 sub EVENT_SAY {
-	if($text=~/trades/i) {
+	if($text=~/hail/i){
+		quest::say("I've little time for banter, unless you have come to me for training as a [new warrior] of the Bloodsabers move along.");
+	}
+	elsif($text=~/new warrior/i){
+		quest::say("A warrior want to be is more like it. You've got a lot of work to do if you're going to be a valued member of the temple of Bertoxxulous, the Plague Bringer. That pompous ruler Antonius Bayle IV has this city in a tight grip with the support of our enemies, the temples of Rodcet Nife and Karana. It is not safe for us to walk the streets of Qeynos openly. You must learn to hold your tongue when not within the walls of our temple here in the Qeynos Catacombs. Should the Qeynos Guards discover your devotion to the Plague Bringer they would surely execute you. We have enemies all about and you must [learn to defend] yourself.");
+	}
+	elsif($text=~/learn to defend/i){
+		quest::say("Take this note to Illie Roln. She will help get you outfitted in a suit of armor. Once you have been properly outfitted return to me and I will tell you how you can make yourself useful. I have a [small task] in mind.");
+		quest::summonitem(20205);
+	}
+	elsif($text=~/trades/i) {
 		quest::say("I thought you might be one who was interested in the various different trades, but which one would suit you? Ahh, alas, it would be better to let you decide for yourself, perhaps you would even like to master them all! That would be quite a feat. Well, lets not get ahead of ourselves, here, take this book. When you have finished reading it, ask me for the [second book], and I shall give it to you. Inside them you will find the most basic recipes for each trade. These recipes are typically used as a base for more advanced crafting, for instance, if you wished to be a smith, one would need to find some ore and smelt it into something usable. Good luck!");
 		quest::summonitem(51121);
 	}
-	if($text=~/second book/i)	{
+	elsif($text=~/second book/i)	{
 		quest::say("Here is the second volume of the book you requested, may it serve you well!");
 		quest::summonitem(51122);
 	}
