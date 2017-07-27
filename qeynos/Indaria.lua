0cdf6fdd34cfd86c8832c83bfc533e9110e443e9
proxeeus <proxeeus@gmail.com>
2017-07-27 08:44:34 +0200

    cosmetic fix for Indaria.lua dialogue

--- qeynos/Indaria.lua
+++ qeynos/Indaria.lua
@@ -9,7 +9,7 @@ end
 function event_trade(e)
   local item_lib =require("items");
   if(item_lib.check_turn_in(e.trade, {item1 = 13712})) then
-    e.self:Emote("tries to keep her composure. She hands you a silver locket with great effort, and then bursts into tears.");
+    e.self:Emote("tries to keep her composure. She hands you a velvet choker with great effort, and then bursts into tears.");
     e.other:SummonItem(1316);
     e.other:Ding();
     e.other:Faction(135,2,0); -- Guards of Qeynos
