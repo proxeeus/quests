e9f3aa0a0776121a2a7250f2c3eb581e777d9f28
proxeeus <proxeeus@gmail.com>
2017-08-02 15:29:13 +0200

    fixed Sir_Lucan_D-Lere.lua : missing classic quest text, rewards, cosmetic dialog fixes

--- freportw/Sir_Lucan_D-Lere.lua
+++ freportw/Sir_Lucan_D-Lere.lua
@@ -1,6 +1,6 @@
 function event_say(e)
 	if(e.message:findi("hail")) then
-		e.self:Say("Hail. citizen!  I am very busy. I do not have time to speak with you.  If you have any complaints. please direct them to City Hall.  Unless. of course. you have some personal business in which I might be interested.");
+		e.self:Say("Hail, citizen!  I am very busy. I do not have time to speak with you.  If you have any complaints, please direct them to City Hall.  Unless, of course, you have some personal business in which I might be interested.");
 	elseif(e.message:findi("shrine of innoruuk")) then
 		e.self:Say("It is about time Pietro sent someone. You people should be lucky I allow you to conduct your business in this city. I will have you all locked up if you cannot be more prompt. Now, listen up! I want you to go out to the Commonlands. My guards have reported that a messenger from Qeynos is staying at an inn near the western edge of the Commonlands. I suspect he is carrying vital information to the Hall of Truth. I want him stopped. Bring me the message if you know what's good for you and the Dismal Rage. Now, get out of here!");
 	end
@@ -22,12 +22,13 @@ function event_trade(e)
 		e.other:Faction(258,-20,0);
 		e.other:AddEXP(5000);
 	elseif(item_lib.check_turn_in(e.trade, {item1 = 13926})) then
-		e.self:Say("Thank you for your loyalty"); -- need live text and reward
+		e.self:Say("So, it was Alayle! He was of no importance to us. He knew nothing. I thank you for assisting the Freeport Militia. My men shall keep an eye out for you. Unfortunately, so will the Knights of Truth. Might I suggest you spend less time in North Freeport?"); 
 		e.other:Ding();
 		e.other:Faction(105,10,0);
 		e.other:Faction(48,10,0);
 		e.other:Faction(184,-40,0);
 		e.other:Faction(258,-40,0);
+		e.other:GiveCash(0, 0, 0, 7);
 		e.other:AddEXP(7500);
 	end
 	item_lib.return_items(e.self, e.other, e.trade);
