6b2a4684ad690f0ee9e1832826d6e524b1975a2b
proxeeus <proxeeus@gmail.com>
2017-09-27 14:55:25 +0200

    implement Trump's skull quest

--- qcat/Kurne_Rextula.pl
+++ qcat/Kurne_Rextula.pl
@@ -11,6 +11,15 @@ sub EVENT_SAY {
   if($text=~/compromise/i) {
 quest::say("The problem is, he was strong in Karana's faith. And as sometimes is the case, there is a resonance from his deity. This causes him to wander in and out of our hidden shrine and I believe it also interferes with my control of the other pets. Please find him and destroy him. Bring me back the note I placed upon his body. Go!!");
   }
+  if($text=~/new task/i) {
+  quest::say("I have been working with a man named Lord Grimrot. We are attempting to raise an army of undead in the Plains of Karana. When the time is right, we shall attack from within as well as from beyond the walls of Qeynos. This army needs one more commander. I must find a Bloodsaber to [retrieve the commander].");
+  }
+  if($text=~/retrieve the commander/i) {
+  quest::say("I have searched for the appropriate man... Sir Lucan of Freeport, Field Marshal Ralem, Marshal Anrey of Rivervale, even Commander Kane Bayle. Any of them would do, but they are either inaccessible or share a common goal with us. So I have decided Trumpy Irontoe is the one. Any former member of the [Irontoe Brigade] has a wealth of military strategies. Kill him and bring me his head. Kane will not be pleased, but, it must be done.");
+  }
+  if($text=~/irontoe brigade/i) {
+  quest::say("The Irontoe Brigade was a legendary military unit hailing from the dwarven city of Kaladim. After a dangerous assault, where more than three quarters of the unit met their bloody fate, the brigade disbanded and left for parts unknown. In Fish's Bar, in Qeynos, you can find one of them, Trumpy Irontoe.");
+  }
 }
 
 sub EVENT_ITEM {
@@ -25,6 +34,18 @@ sub EVENT_ITEM {
     quest::faction(257,-1);
     quest::faction(53,1);
     quest::givecash(0,3,0,0);
+  }
+  if (plugin::check_handin(\%itemcount, 12136 => 1)) {	#Trumpy's head
+		quest::say("Incredible!! You have slain one of the greatest warriors in Qeynos!! He must have been full of grog. No doubt he drank most of his skill away. Now I shall cast a spell and strip the flesh from his skull and.. Presto!! Take this skull to Lord Grimrot somewhere in the Plains of Karana. He will be in the center of a field of skeletons. If he is not there, wait for his return. He must return eventually.");
+		quest::summonitem(12137);
+		quest::ding();
+		quest::exp(10000);
+		quest::faction(21,15);
+		quest::faction(135,-10);
+		quest::faction(235,-10);
+		quest::faction(257,-10);
+		quest::faction(53,10);
+		quest::givecash(5,3,0,0);
   }
   plugin::return_items(\%itemcount);
 }
