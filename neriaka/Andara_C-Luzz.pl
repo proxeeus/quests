bc64e5152529fb6dfdd99f856c62a04a5ac1c621
proxeeus <proxeeus@gmail.com>
2017-11-04 18:41:20 +0100

    fixes for the Ebon Bow newbie quest (dialogue not classic but flavorful)

--- neriaka/Andara_C-Luzz.pl
+++ neriaka/Andara_C-Luzz.pl
@@ -4,10 +4,11 @@
 #Status: Not Complete 
 
 sub EVENT_ITEM { 
-if(($itemcount{16166} == 1) && ($itemcount{19582} == 1) && ($itemcount{19583} == 1)) 
-  { 
-   quest::ding(); quest::exp(150); 
-  quest::summonitem(19611); 
-  quest::say("Here's your bow.  Use it well, young rogue."); 
-  } 
+	if (plugin::check_handin(\%itemcount, 16166 => 1, 19582 => 1, 19583 => 1)) 
+	{
+		quest::say("Fine work, $name. You do the Hall of the Ebon Mask proud on this day. Here, take this weapon. Do not hesitate to test it on more of those pesky halflings, HA ! All Hail Innoruuk, our Lord and Creator!"); 
+		quest::ding();
+		quest::exp(1500);		
+		quest::summonitem(19611); 
+	} 
 } 
