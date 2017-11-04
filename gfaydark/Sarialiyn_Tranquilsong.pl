0e4e016193721104d70dbc8330d568cd84616afe
proxeeus <proxeeus@gmail.com>
2017-11-04 13:10:27 +0100

    cosmetic fixes redux

--- gfaydark/Sarialiyn_Tranquilsong.pl
+++ gfaydark/Sarialiyn_Tranquilsong.pl
@@ -13,31 +13,31 @@ quest::say("Fantastic $name. here is your Melodic Assembly Kit. In this kit you 
 quest::summonitem(17368);
 }
 if($text=~/bracers/i){
-quest::say("To create your bracer material you will need to combine 1 Brick of Crude Bronze, 1 Giant Wasp Venom Sac. 1 Basilisk Hatchling Hide and 1 Bandage in your assembly kit. Once you have created the proper material take it to a forge along with this mold to fashion your very own Bracer of the Tranquilsong.");
+quest::say("To create your bracer material you will need to combine 1 Brick of Crude Bronze, 1 Giant Wasp Venom Sac, 1 Basilisk Hatchling Hide and 1 Bandage in your assembly kit. Once you have created the proper material take it to a forge along with this mold to fashion your very own Bracer of the Tranquilsong.");
 quest::summonitem("22611");
 }
 if($text=~/helms/i){
-quest::say("To create your helm material you will need to combine 2 Bricks of Crude Bronze, 1 Faerie Wing. 1 Royal Jelly and 1 Mead in your assembly kit. Once you have created the proper material take it to a forge along with this mold to fashion your very own Helm of the Tranquilsong.");
+quest::say("To create your helm material you will need to combine 2 Bricks of Crude Bronze, 1 Faerie Wing, 1 Royal Jelly and 1 Mead in your assembly kit. Once you have created the proper material take it to a forge along with this mold to fashion your very own Helm of the Tranquilsong.");
 quest::summonitem("22610");
 }
 if($text=~/armguards/i){
-quest::say("To create your armguards material you will need to combine 2 Bricks of Crude Bronze, 2 Wasp Wings. 1 Large Widow Abdomen and 1 Medium Quality Wolf Skin in your assembly kit. Once you have created the proper material take it to a forge along with this mold to fashion your very own Armguards of the Tranquilsong.");
+quest::say("To create your armguards material you will need to combine 2 Bricks of Crude Bronze, 2 Wasp Wings, 1 Large Widow Abdomen and 1 Medium Quality Wolf Skin in your assembly kit. Once you have created the proper material take it to a forge along with this mold to fashion your very own Armguards of the Tranquilsong.");
 quest::summonitem("22613");
 }
 if($text=~/boots/i){
-quest::say("To create your boot material you will need to combine 3 Bricks of Crude Bronze, 1 Severed Orc Foot. 2 Pixie Wings and 1 Brownie Parts and 1 Silk Thread in your assembly kit. Once you have created the proper material take it to a forge along with this mold to fashion your very own Boots of the Tranquilsong.");
+quest::say("To create your boot material you will need to combine 3 Bricks of Crude Bronze, 1 Severed Orc Foot, 2 Pixie Wings, 1 Brownie Parts and 1 Silk Thread in your assembly kit. Once you have created the proper material take it to a forge along with this mold to fashion your very own Boots of the Tranquilsong.");
 quest::summonitem("22612");
 }
 if($text=~/greaves/i){
-quest::say("To create your greaves material you will need to combine 4 Bricks of Crude Bronze, 1 Orc Skull. 1 Orc Scalp and 1 Orc Fibula in your assembly kit. Once you have created the proper material take it to a forge along with this mold to fashion your very own Greaves of the Tranquilsong.");
+quest::say("To create your greaves material you will need to combine 4 Bricks of Crude Bronze, 1 Orc Skull, 1 Orc Scalp and 1 Orc Fibula in your assembly kit. Once you have created the proper material take it to a forge along with this mold to fashion your very own Greaves of the Tranquilsong.");
 quest::summonitem("22614");
 }
 if($text=~/gauntlets/i){
-quest::say("To create your gauntlet material you will need to combine 3 Bricks of Crude Bronze, 1 Spider Legs. 1 Orc Sternum. 1 Orc Head and 1 Orc Humerus in your assembly kit. Once you have created the proper material take it to a forge along with this mold to fashion your very own Gauntlets of the Tranquilsong.");
+quest::say("To create your gauntlet material you will need to combine 3 Bricks of Crude Bronze, 1 Spider Legs, 1 Orc Sternum, 1 Orc Head and 1 Orc Humerus in your assembly kit. Once you have created the proper material take it to a forge along with this mold to fashion your very own Gauntlets of the Tranquilsong.");
 quest::summonitem("22615");
 }
 if($text=~/breastplates/i){
-quest::say("To create your breastplate material you will need to combine 5 Bricks of Crude Bronze, 1 Orc Fibula. 1 Pristine Forest Drakeling Scale. 1 Golden Bandit Tooth. 1 Orc Eye and 1 Orc Ribcage in your assembly kit. Once you have created the proper material take it to a forge along with this mold to fashion your very own Breastplate of the Tranquilsong. Once you have completed your armor set please return to me for I have one final exercise for you to complete.");
+quest::say("To create your breastplate material you will need to combine 5 Bricks of Crude Bronze, 1 Orc Fibula, 1 Pristine Forest Drakeling Scale, 1 Golden Bandit Tooth, 1 Orc Eye and 1 Orc Ribcage in your assembly kit. Once you have created the proper material take it to a forge along with this mold to fashion your very own Breastplate of the Tranquilsong. Once you have completed your armor set please return to me for I have one final exercise for you to complete.");
 quest::summonitem("22616");
 }
 if($text=~/final exercise/i){
