f50b9d1f0725e868e4af9e202424d416a09c0b35
proxeeus <proxeeus@gmail.com>
2017-11-05 18:49:23 +0100

    newbie DE warrior quest fixes

--- neriakb/Issva_H-Rugla.pl
+++ neriakb/Issva_H-Rugla.pl
@@ -26,13 +26,22 @@ sub EVENT_SAY {
 		quest::summonitem(19560);#Tattered Pant Pattern
   }
   if($text=~/vambraces/i) {
-		quest::say("To craft Ord Slayer Vambraces you require two [silk thread], a pyre beetle carapace, and two orc humerus bones. Once you have the necessary components combine them in your Mail Assembly Kit with this Tattered Sleeves Pattern.");
+		quest::say("To craft Orc Slayer Vambraces you require two [silk thread], a pyre beetle carapace, and two orc humerus bones. Once you have the necessary components combine them in your Mail Assembly Kit with this Tattered Sleeves Pattern.");
 		quest::summonitem(19557);#Tattered Sleeve Pattern
   }
   if($text=~/breastplate/i) {
 		quest::say("To craft an Orc Slayer Breastplate you require four [silk thread], a pristine pyre beetle carapace, an orc sternum, and an orc ribcage. Once you have the necessary components combine them in your Mail Assembly Kit with this Tattered Tunic Pattern.");
 		quest::summonitem(19556);#Tattered Tunic Pattern
   }
+  	if($text=~/loom/i){
+		quest::say("A sewing kit may be purchased at the Furrier Royale in Neriaks Third Quarter or at the Silk Underground in Neriaks Foreign Quarter. A community loom may be found at the Silk Underground in Neriaks Foreign Quarter.");
+	}
+	if($text=~/sewing kit/i){
+		quest::say("A sewing kit may be purchased at the Furrier Royale in Neriaks Third Quarter or at the Silk Underground in Neriaks Foreign Quarter. A community loom may be found at the Silk Underground in Neriaks Foreign Quarter.");
+	}
+	if($text=~/silk thread/i){
+		quest::say("Silk Thread is sewn by a tailor using a [sewing kit] or a community [loom]. Two spiderling silks combined in the sewing kit or loom will create one silk thread.");
+	}
 }
 sub EVENT_ITEM{
 	if(plugin::check_handin(\%itemcount, 31753 => 1)) {#Request Form
--- neriakb/Jarrex_N-Ryt.pl
+++ neriakb/Jarrex_N-Ryt.pl
@@ -7,13 +7,8 @@ sub EVENT_SAY{
     quest::say("What have we here? A new weakling that wishes to reap some sort of self-value and renown through service to the Indigo Brotherhood perhaps? Well speak up whelp!! Have you come to [destroy the enemies of Neriak] or are you a coward better suited to mop the filth from the back rooms of the Maidens Fancy.");
   }
   if($text=~/destroy the enemies of Neriak/i) {
-  	if (($class eq "Warrior") && ($race eq "Dark Elf")){
     	quest::say("If you are going to be of any value to the Indigo Brotherhood you will need to outfit yourself in a suit of armor that will turn aside the blades of enemy forces. From the looks of you even the tiny fangs of a moss snake could bring you death. Take this request form to Issva H'Rugla to get yourself started on the assembly of your armor. When you have procured some protection for your weakling body return to me for [further instruction].");
     	quest::summonitem(31753);#Request Form
-    }
-    else{
-    	quest::say("Begone fool, you are no member of the Indigo Brotherhood!");#text made up
-    }
   }
   if($text=~/further instruction/i) {
     quest::say("Ah, you are eager to advance further within the Indigo Brotherhood. Although it is my duty to aid your training, do not allow your arrogance to blind you to your lowly position amongst the Kings most loyal subjects. Your next task is to assist the construction of a [weapon] and [shield] worthy of being wielded by a Warrior of the Indigo Brotherhood.");
@@ -26,27 +21,13 @@ sub EVENT_SAY{
   }
 }
 sub EVENT_ITEM{
-	if(plugin::check_handin(\%itemcount, 13791 => 1, 19565 => 1, 19564 => 1)) {#Orc Scalp, Orc Clavicle, Orc Fibula
-		if (($class eq "Warrior") && ($race eq "Dark Elf")){
-			quest::say("Excellent.  Here is your weapon, $name.  Use it to slay more!");#text made up
+	if(plugin::check_handin(\%itemcount, 13791 => 1, 19565 => 1, 19564 => 1)) {	#Orc Scalp, Orc Clavicle, Orc Fibula
+			quest::say("Excellent work $name. You do the Indigo Brotherhood proud on this day. Take this weapon, and wield it in the name of Innoruuk, our Lord and Creator!");#text made up
 			quest::summonitem(19606);#Jagged Orc Slayer Sword
-		}
-		else {
-			quest::say("I don't need this."); #text made up
-			plugin::return_items(\%itemcount);
-			return 1;
-		}
 	}
-	elsif(plugin::check_handin(\%itemcount, 19562 => 4)) {#Young Kodiak Femurs
-		if (($class eq "Warrior") && ($race eq "Dark Elf")){
-			quest::say("So you have more strength than you seem to.  Hmm...here is your shield, $name.");#text made up
-			quest::summonitem(19609);#Black Femur Shield
-		}
-		else{
-			quest::say("I don't need this."); #text made up
-			plugin::return_items(\%itemcount);
-			return 1;
-		}	
+	elsif(plugin::check_handin(\%itemcount, 19562 => 4)) {	#Young Kodiak Femurs
+			quest::say("So you have more strength than you seem to. You may yet be useful to the Indigo Brotherhood then... Hmm, as promised...here is your shield, $name. Wield it with pride.");#text made up
+			quest::summonitem(19609);#Black Femur Shield	
 	}
   else {
   	plugin::try_tome_handins(\%itemcount, $class, 'Warrior');
