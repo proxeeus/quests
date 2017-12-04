129c5cf2bcef7a03564c30765a001457926c3847
proxeeus <proxeeus@gmail.com>
2017-12-04 15:15:22 +0100

    Update Corporal_Lancot.lua

--- qeynos2/Corporal_Lancot.lua
+++ qeynos2/Corporal_Lancot.lua
@@ -11,7 +11,8 @@ function event_combat(e)
 	e.self:Say( eq.ChooseRandom( string.format("It's %s like you who have ruined your own lands. You'll not ruin mine!",racesplural.GetPlural(e.other:GetRace())), 
 								 string.format("%s have no place in our realm!",racesplural.GetPlural(e.other:GetRace())),
 								 string.format("%s like you are better left dead than alive!",racesplural.GetPlural(e.other:GetRace())),
-								 string.format("Time to die %s.",racesplural.GetSingle(e.other:GetRace()))
+								 string.format("Time to die %s.",racesplural.GetSingle(e.other:GetRace())),
+								"Halt! You are an enemy of the people of Qeynos! Stop in the name of Antonius Bayle and prepare for punishment!"
 								)
 			   );
 
