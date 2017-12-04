909260b1cf2fab6fd435037fd9db7f4bcfa6c14e
proxeeus <proxeeus@gmail.com>
2017-12-04 15:09:27 +0100

    Create Gorenaire.lua

---
+++ dreadlands/Gorenaire.lua
@@ -0,0 +1,7 @@
+function event_combat(e)
+  e.self:Shout("Why must your mortals always hunt my people like dogs! Let your deaths be a lesson to those who are wiser than to defy the will of Veeshan!");
+end
+
+function event_slay(e)
+  e.self:Shout("Another lost soul has found its way home. I pray you live your next life a bit more wisely.");
+end
