1149a49cb19933f65ac7c3b3822f0c5e12a99a93
proxeeus <proxeeus@gmail.com>
2017-12-04 13:46:41 +0100

    Update Guard_McCluskey.pl

--- qey2hh1/Guard_McCluskey.pl
+++ qey2hh1/Guard_McCluskey.pl
@@ -1,6 +1,19 @@
-sub EVENT_SAY { 
-if($text=~/Hail/i){
-quest::say("How are you? You'd better have a good reason for bothering me. I am a busy man."); }
+sub EVENT_SAY
+{ 
+  if($text=~/hail/i)
+  {
+    quest::say("How are you? You'd better have a good reason for bothering me. I am a busy man.");
+  }
 }
+
+sub EVENT_COMBAT
+{
+  if($combat_state == 1)
+  {
+    quest::say("Time to die, $name.");
+    quest::say("Your intolerable reputation insults all this realm!");
+  }
+}
+
 #END of FILE Zone:qey2hh1  ID:2306 -- Guard_McCluskey 
 
