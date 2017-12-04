435421d5215e72f73e68b58ac4f40356fc21bf2e
proxeeus <proxeeus@gmail.com>
2017-12-04 15:29:13 +0100

    Update Guard_Dinler.lua

--- kaladima/Guard_Dinler.lua
+++ kaladima/Guard_Dinler.lua
@@ -5,3 +5,7 @@ end
 function event_death_complete(e)
 	e.self:Say("My comrades will avenge my death.");
 end
+
+function event_slay(e)
+	e.self:Say("For the glory of Kaladim!! You are no more.");
+end
