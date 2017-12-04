0d36e294ef49569c13560bd9ce879981b0d118db
proxeeus <proxeeus@gmail.com>
2017-12-04 13:42:13 +0100

    Update Guard_Kilson.lua

--- qey2hh1/Guard_Kilson.lua
+++ qey2hh1/Guard_Kilson.lua
@@ -1,5 +1,6 @@
 function event_combat(e)
 	if(e.joined == true) then
-		e.self:Say(string.format("Time to die %s!",e.other:GetCleanName()));
+		e.self:Say(string.format("Time to die, %s!",e.other:GetCleanName()));
+		e.self:Say("Your intolerable reputation insults all this realm!");
 	end
 end
