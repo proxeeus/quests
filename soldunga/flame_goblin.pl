9888f666a92452089524aeb8edefd5e32cc7bc6f
proxeeus <proxeeus@gmail.com>
2017-12-04 13:25:03 +0100

    Update flame_goblin.pl engagement text

--- soldunga/flame_goblin.pl
+++ soldunga/flame_goblin.pl
@@ -1,6 +1,9 @@
-sub EVENT_SAY { 
-if($text=~/i am INVULNERABLE do not heal me!/i){
-quest::say("Die by lava - Die by flame - Fire Peak goblins kill and maim."); }
+sub EVENT_COMBAT
+{
+  if($combat_state == 1)
+  {
+    quest::say("Die by lava - Die by flame - Fire Peak goblins kill and maim."); 
+  }
 }
 #END of FILE Zone:soldunga  ID:31053 -- flame_goblin 
 
