f14e6579c314e19b2dae2bcd4146eadcafc5638b
proxeeus <proxeeus@gmail.com>
2017-12-04 13:44:52 +0100

    Update Guard_Donlan.pl

--- qey2hh1/Guard_Donlan.pl
+++ qey2hh1/Guard_Donlan.pl
@@ -1,6 +1,18 @@
-sub EVENT_SAY { 
-if($text=~/Hail/i){
-quest::say("Welcome. traveler. Have I informed you about the travel tax?"); }
+sub EVENT_SAY 
+{ 
+  if($text=~/hail/i)
+  {
+    quest::say("Welcome, traveler. Have I informed you about the travel tax?"); 
+  }
+}
+
+sub EVENT_COMBAT
+{
+  if($combat_state == 1)
+  {
+    quest::say("Time to die, $name.");
+    quest::say("Your intolerable reputation insults all this realm!");
+  }
 }
 #END of FILE Zone:qey2hh1  ID:2179 -- Guard_Donlan 
 
