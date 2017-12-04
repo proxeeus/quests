58cc6051289ae6cd55643cfff946051c88ee4501
proxeeus <proxeeus@gmail.com>
2017-12-04 13:39:38 +0100

    Update a_bandit.lua

--- qey2hh1/a_bandit.lua
+++ qey2hh1/a_bandit.lua
@@ -3,3 +3,7 @@ function event_combat(e)
     e.self:Say(string.format("Time to die, %s!",e.other:GetCleanName()));
   end
 end
+
+function event_death_complete(e)
+    e.self:Say("My comrades will avenge my death.");
+end
