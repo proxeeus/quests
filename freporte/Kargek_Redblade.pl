7c6b07bb654328041324911703e95eeddc5e795e
proxeeus <proxeeus@gmail.com>
2017-12-05 15:48:41 +0100

    Update Kargek_Redblade.pl

--- freporte/Kargek_Redblade.pl
+++ freporte/Kargek_Redblade.pl
@@ -1,43 +1,5 @@
-############################################
-# ZONE: East Freeport (freeporte)
-# DATABASE: Cavedude + PEQ (from server release 3.52)
-# LAST EDIT DATE: 5/1/06
-# VERSION: 1.0
-# BASE QUEST BY: PEQ Team
-# DEVELOPER: MWMDRAGON
-# EDITED BY: ATTEMPT37
-#
-# *** NPC INFORMATION ***
-#
-# NAME: Kargek_Redblade
-# ID: 10117
-# TYPE: Warrior
-# RACE: Human
-# LEVEL: 50
-#
-# *** ITEMS GIVEN OR TAKEN ***
-#
-# Wax Sealed Note ID-20673
-# Tiny Lute ID-20674
-# Redblade's Legacy ID- 18083
-# Tanel's Note to Kargek ID-20684
-# Spiroc Wingblade ID-20679
-# Red Scabbard ID-17859
-#
-# *** QUESTS INVOLVED IN ***
-#
-#1 - Jagged Blade of War - Warrior Epic
-#
-#
-#
-#
-############################################
-
-######## EVENT_SAY AREA ####################
-### Called when the NPC is spoken to by a PC
-
-sub EVENT_SAY {
-  if($class eq "Warrior") {
+sub EVENT_SAY
+ {
     if($text=~/Hail/i)   {
       quest::emote("looks up from his forge and says, 'Hello there, Can I help you?' Your eyes are drawn to an extremely large [red sword Scabbard] strapped to his back with no sword in it.");
       quest::say("I said, can I help you or do you need help from [Wenden]?");
@@ -52,7 +14,7 @@ sub EVENT_SAY {
       quest::say("An associate of mine is wandering somewhere in the Feerrott. His name is Oknoggin Stonesmacker. If you will Deliver this note to him, I would appreciate it.");
       quest::summonitem("20673"); # Wax Sealed Note ID-20673
     }
-  }
+
   if($text=~/Korbuk Brimblade sent me to help/i){
     quest::say("Really? I thought we'd have to deal with this problem on our own, and that has proven most distressful. If you really are here on word from Korbuk, you'll have no problem helping my family recover a [sword] we made and put on our father's grave.");
   }
@@ -66,9 +28,6 @@ sub EVENT_SAY {
     quest::say("If you can catch him and return the sword he stole, we can once again place it on the grave of my father where it belongs. He'll be expecting me or someone in lieu of me to try to find him, so you'll have to wait for him during the evening, when he's most likely to come out of hiding. He hasn't had a lot of time to get out of the area, so you can probably find him near my father's grave, in the eastern plains of Karana.");
   }
 }
-
-######## EVENT_ITEM AREA ###################
-### Called when the NPC is handed items
 
 sub EVENT_ITEM {
   if (plugin::check_handin(\%itemcount, 20674 => 1)) { # Tiny Lute
@@ -93,4 +52,3 @@ sub EVENT_ITEM {
 }
 
 #END of FILE Zone:freporte  ID:10117 -- Kargek_Redblade
-
