4d08db40fcdbee9b839bc5018a5960641e981c53
proxeeus <proxeeus@gmail.com>
2017-12-05 15:23:41 +0100

    Update Elisi_Nasin.pl

--- freporte/Elisi_Nasin.pl
+++ freporte/Elisi_Nasin.pl
@@ -1,52 +1,14 @@
-############################################
-# ZONE: East Freeport (freporte)
-# DATABASE: PEQ-Velious
-# LAST EDIT DATE: May 1,2005
-# VERSION: 1.0
-# BASE QUEST BY: PEQ Team
-# DEVELOPER: MWMDRAGON
-#
-# *** NPC INFORMATION ***
-#
-# NAME: Elisi_Nasin
-# ID: 10108
-# TYPE: Rogue Guild Master
-# RACE: Human
-# LEVEL: 61
-#
-# *** ITEMS GIVEN OR TAKEN ***
-#
-# A tattered note ID-18745
-# Brown Faded Tunic ID-13568
-#
-# *** QUESTS INVOLVED IN ***
-#
-#1 - Newbie Note
-#
-# *** QUESTS AVAILABLE TO ***
-#
-#1 - Rogue
-#
-# *** NPC NOTES ***
-#
-#
-#
-############################################
-
-
 sub EVENT_SAY
 { 
+      if($text=~/Hail/i)
+      {
+        quest::say("Listen. this is a pretty busy [operation] I run here.  I certainly don't have any spare time to sit around and chit-chat with you all day, got it?");
+      }
 
-if($text=~/Hail/i)
-{
-quest::say("Listen. this is a pretty busy [operation] I run here.  I certainly don't have any spare time to sit around and chit-chat with you all day. got it?");
-}
-
-if($text=~/operation/i)
-{
-quest::say("Look. $name . I don't have time for this.  Let's just say importing exotic goods is our main interest. along with a few other profitable ventures.  Now quit pestering me. or you'll force me to sic Grenny here on ya.");
-}
-
+      if($text=~/operation/i)
+      {
+        quest::say("Look, $name, I don't have time for this.  Let's just say importing exotic goods is our main interest, along with a few other profitable ventures.  Now quit pestering me, or you'll force me to sic Grenny here on ya.");
+      }
 }
 
 sub EVENT_ITEM
