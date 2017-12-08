a7724447d2e5cfca81ff5b98e573a7014575a0dd
proxeeus <proxeeus@gmail.com>
2017-12-08 15:59:44 +0100

    Create Masurt_Dok.lua

---
+++ iceclad/Masurt_Dok.lua
@@ -0,0 +1,5 @@
+function event_say(e)
+	if(e.message:findi("hail")) then
+    e.self:Say("That's me name, me job too. 'Masert ub da dok', as a mate once called me. Course I was the bosun at the time, and we weren't at a dock, but he was drunk. Captain Nalot's Triple Strength will do that to ya.");
+  end
+end
