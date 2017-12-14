bd05ee3dae23872282769af743f3d86e85d3073b
proxeeus <proxeeus@gmail.com>
2017-12-14 13:05:22 +0100

    Update Phin_Esrinap.lua

--- qeynos2/Phin_Esrinap.lua
+++ qeynos2/Phin_Esrinap.lua
@@ -46,6 +46,9 @@ function event_trade(e)
 		e.other:AddEXP(100);
 	-- Yellow Headband ID-10111 requires three Putrid Rib Bone ID-13722 and the White Headband ID-10110
 	elseif(item_lib.check_turn_in(e.trade, {item1 = 13722,item2 = 13722, item3 = 13722,item4 = 10110})) then
+		e.self:Say("Ahh.. Good. I feel that your hard work and training will soon be rewarded.. and the yellow headband shall soon be yours.");
+		e.self:Say("Ahh.. Good. I feel that your hard work and training will soon be rewarded.. and the yellow headband shall soon be yours.");
+		e.self:Say("Ahh.. Good. I feel that your hard work and training will soon be rewarded.. and the yellow headband shall soon be yours.");
 		e.self:Say("Good work.. and, as promised, here is your reward. It is an honor to present this yellow headband to " .. e.other:GetName() .. ", for recent acts of courage and heroism, on behalf of the Silent Fist Clan.");
 		e.other:SummonItem(10111);
 		e.other:Ding();
@@ -55,6 +58,9 @@ function event_trade(e)
 		e.other:AddEXP(200);
 	-- Orange Headband ID-10112 requires two Blackburrow Gnoll Pelt ID-13027, one Blackburrow Gnoll Skin ID-13028 and the Yellow Headband ID-10111
 	elseif(item_lib.check_turn_in(e.trade, {item1 = 13027,item2 = 13027, item3 = 13028,item4 = 10111})) then
+		e.self:Say("Good work, " .. e.other:GetName() .. ". You are well on your way toward earning the orange headband.");
+		e.self:Say("Good work, " .. e.other:GetName() .. ". You are well on your way toward earning the orange headband.");
+		e.self:Say("Good work, " .. e.other:GetName() .. ". You are well on your way toward earning the orange headband.");
 		e.self:Say("It is an honor to present the orange headband of the Silent Fist Clan to one of our finest members, " .. e.other:GetName() .. " the mighty!");
 		e.other:SummonItem(10112);
 		e.other:Ding();
