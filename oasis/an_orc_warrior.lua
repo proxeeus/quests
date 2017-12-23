9068c2194b5fbf61b3642e849ac311a0b8765f21
proxeeus <proxeeus@gmail.com>
2017-12-23 17:06:27 +0100

    flavor for Dervs / Orcs / Madmen in sro/nro/oasis

--- oasis/an_orc_warrior.lua
+++ oasis/an_orc_warrior.lua
@@ -4,18 +4,13 @@ function event_combat(e)
 
 	e.self:Say( eq.ChooseRandom( string.format("It's %s like you who have ruined your own lands. You'll not ruin mine!",racesplural.GetPlural(e.other:GetRace())), 
 								 string.format("%s have no place in our realm!",racesplural.GetPlural(e.other:GetRace())),
+								 "Orc stomp, orc kill - orc weapons, your blood will spill.",
 								 string.format("%s like you are better left dead than alive!",racesplural.GetPlural(e.other:GetRace())),
 								 string.format("Time to die %s.",racesplural.GetSingle(e.other:GetRace()))
 								)
 			   );
 
 end
-
-function event_spawn(e)
-	local randomface = require("random_face");
-	randomface.RandomFace(e.self, eq);
-end
-
 
 function event_death_complete(e)
 	e.self:Say("My comrades will avenge my death.");
