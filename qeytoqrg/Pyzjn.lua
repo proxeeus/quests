781c5c6e21fe5236d4eab1f1655701d9dca77472
proxeeus <proxeeus@gmail.com>
2018-01-04 13:02:51 +0100

    Update Pyzjn.lua

--- qeytoqrg/Pyzjn.lua
+++ qeytoqrg/Pyzjn.lua
@@ -1,3 +1,7 @@
+-- Hailing should test against faction.
+-- If friendly :Pyzjn says 'Hail, Arkytz . Pyzjn is working for Master Varsoon. If Pyzjn do good work maybe Master Varsoon tell Master Bruax he is worthy. Pyzjn must go now. All glory to the Plaguebringe! May you die a painful, oozing death.
+-- otherwise keep the same text
+
 function event_say(e)
 	e.self:Say("I didn’t know Slime could speak common..go back to the sewer before I lose my temper.");
 end
