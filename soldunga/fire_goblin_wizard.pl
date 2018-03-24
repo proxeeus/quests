5b400e36cf27e4bcb1068ce981896ea5e9fcfc6d
proxeeus <proxeeus@gmail.com>
2018-03-24 20:22:03 +0100

    bit of flavor to gobs in soldunga

--- soldunga/fire_goblin_wizard.pl
+++
@@ -1,7 +0,0 @@
-sub EVENT_COMBAT
-{
-  if($combat_state == 1)
-  {
-    quest::say("Die by lava - Die by flame - Fire Peak goblins kill and maim."); 
-  }
-}
--- soldunga/flame_goblin.pl
+++
@@ -1,9 +0,0 @@
-sub EVENT_COMBAT
-{
-  if($combat_state == 1)
-  {
-    quest::say("Die by lava - Die by flame - Fire Peak goblins kill and maim."); 
-  }
-}
-#END of FILE Zone:soldunga  ID:31053 -- flame_goblin 
-
