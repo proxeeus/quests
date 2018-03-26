28034380365df6eed97a3e910020b31723083485
proxeeus <proxeeus@gmail.com>
2018-03-26 12:48:04 +0200

    Update Caskin_Marsheart.lua

--- freportn/Caskin_Marsheart.lua
+++ freportn/Caskin_Marsheart.lua
@@ -2,7 +2,7 @@ function event_trade(e)
 	local item_lib = require("items");
 
 	if(item_lib.check_turn_in(e.trade, {item1 = 18747})) then
-		e.self:Say("Welcome to the guild, here's your guild tunic. Now. let's get to work.");
+		e.self:Say("Welcome to the guild, here's your guild tunic. Now, let's get to work.");
 		e.other:SummonItem(13571);
 		e.other:Ding();
 		e.other:Faction(192,100,0);
