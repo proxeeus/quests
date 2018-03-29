804efe8c6eec38124bbb37bd63236683c5008c34
proxeeus <proxeeus@gmail.com>
2018-03-29 16:16:11 +0200

    Update Hollish_Tnoops.lua

--- freportw/Hollish_Tnoops.lua
+++ freportw/Hollish_Tnoops.lua
@@ -1,3 +1,9 @@
 function event_spawn(e)
   e.self:SetRunning(true);
 end
+
+function event_say(e)
+    if(e.message:findi("hail")) then
+      e.self:Say("Hail. I am in a bit of a hurry. Please excuse me.");
+    end
+end
