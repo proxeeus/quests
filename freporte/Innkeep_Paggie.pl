1a43a21d086543513e03b8d6c6a7370e9b46395f
proxeeus <proxeeus@gmail.com>
2018-04-04 16:05:23 +0200

    Update and rename Innkeep_Paggie.pl to Innkeep_Paggie.lua

--- freporte/Innkeep_Paggie.pl
+++
@@ -1,8 +0,0 @@
-sub EVENT_SAY
-{
-   if($text=~/Hail/i)
-   {
-      quest::say("Hello there $name ! You'll be happy to see that I am fully stocked with all my usual goods. Have a look around");
-   }
-}
-
