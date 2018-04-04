f8b0e3c058b94c4a14beadbc1154f85c140c3430
proxeeus <proxeeus@gmail.com>
2018-04-04 13:16:20 +0200

    Update Deputy_Budo.lua

--- misty/Deputy_Budo.lua
+++ misty/Deputy_Budo.lua
@@ -11,7 +11,7 @@ function event_say(e)
     e.self:Say("Beyond this wall lies Runnyeye, the lair of the goblins of Clan Runnyeye. They have been a nuisance to Rivervale for quite some time. If you wish to join the fight, go to Rivervale and ask Sheriff Roglio about Clan Runnyeye.");
   elseif(e.message:findi("reporting for duty")) then
     e.self:Say(string.Format(
-    "Stand tall then, Deputy $name. Your orders are to keep the creatures from venturing into the community of Rivervale and to kill many goblins. Check the goblin bodies, if you should find unbroken warrior beads on them, take them to Sheriff Roglio Bruth and should you be lucky enough to find a bizarre grappling device, you are to take it to Marshal Ghobber Truppack immediately! One more thing - should you defeat any orcs in battle, bring me their leather belts and I shall reward you for the great victory. May the power of Underfoot be with you!"
+    "Stand tall then, Deputy %s. Your orders are to keep the creatures from venturing into the community of Rivervale and to kill many goblins. Check the goblin bodies, if you should find unbroken warrior beads on them, take them to Sheriff Roglio Bruth and should you be lucky enough to find a bizarre grappling device, you are to take it to Marshal Ghobber Truppack immediately! One more thing - should you defeat any orcs in battle, bring me their leather belts and I shall reward you for the great victory. May the power of Underfoot be with you!"
     ,e.other:GetName()));
   end
 end
@@ -25,7 +25,7 @@ function event_trade(e)
 
 	if(item_lib.check_turn_in(e.trade, {item1 = 18911})) then
 		e.self:Say(string.Format(
-      "Grand work. Deputy $name! You will become a full-fledged deputy in no time. You should check in with Marshal Ghobber from time to time to see if you have earned your squad ring. Just go up to him and ask. 'Am I one with the Wall?' He will know. but you must first show your bravery in many battles against the goblins."
+      "Grand work. Deputy %s! You will become a full-fledged deputy in no time. You should check in with Marshal Ghobber from time to time to see if you have earned your squad ring. Just go up to him and ask. 'Am I one with the Wall?' He will know. but you must first show your bravery in many battles against the goblins."
       ,e.other:GetName()));
 		e.other:Ding();
 		e.other:Faction(133,10,0);
