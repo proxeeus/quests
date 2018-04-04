f4b590ef6b6ea01600a3a04a605fc57e8eef618b
proxeeus <proxeeus@gmail.com>
2018-04-04 16:05:50 +0200

    Update and rename Innkeep_Rastle.pl to Innkeep_Rastle.lua

--- freporte/Innkeep_Rastle.pl
+++
@@ -1,8 +0,0 @@
-sub EVENT_SAY
-{
-   if($text=~/Hail/i)
-   {
-      quest::say("Hail $name. Business is going well these days. I have a full stock of baked goods and other items for sale. Have a look around!");
-   }
-}
-
