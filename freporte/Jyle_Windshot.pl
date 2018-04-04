a752db1af6663bf81d84a22403103bf1ab9f2df4
proxeeus <proxeeus@gmail.com>
2018-04-04 16:13:48 +0200

    Update and rename Jyle_Windshot.pl to Jyle_Windshot.lua

--- freporte/Jyle_Windshot.pl
+++
@@ -1,7 +0,0 @@
-sub EVENT_SAY
-{
-   if($text=~/Hail/i)
-   {
-      quest::say("Greetings, friend!!  Nice to meet you.  I see you enjoy barging into other people's rooms.  No problem.  I enjoy the company.  What are you looking for?");
-   }
-}
