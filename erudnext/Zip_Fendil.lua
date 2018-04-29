55a3c2fbf9987c757fb7a97c838f95f3c0416d10
proxeeus <proxeeus@gmail.com>
2018-04-29 17:03:14 +0200

    add Zip_Fendil.lua

---
+++ erudnext/Zip_Fendil.lua
@@ -0,0 +1,9 @@
+
+function event_combat(e)
+	e.self:Say("To death with you!!");
+end
+
+function event_death(e)
+	e.self:Say("You have brought the vengeance of the Circle of the Unseen Hands upon yourself!");
+end
+
