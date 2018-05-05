76a5cd6a3aa1e99a235d387be4324fa4a613088a
proxeeus <proxeeus@gmail.com>
2018-05-05 21:26:48 +0200

    added death message to Chief_Adair.lua

--- erudnint/Chief_Adair.lua
+++ erudnint/Chief_Adair.lua
@@ -1,7 +1,11 @@
 function event_say(e)
 	if(e.message:findi("hail")) then
-		e.self:Say("Hail. citizen!! Have you business with me? If so. please speak quickly and be on your way.");
+		e.self:Say("Hail., citizen!! Have you business with me? If so, please speak quickly and be on your way.");
 	end
+end
+
+function event_death(e)
+	e.self:Say("Prepare to meet the vengeance of Erud!");
 end
 
 -- END of FILE Zone:erudnint  ID:24051 -- Chief_Adair
