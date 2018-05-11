ff1c96e839a811d7b60e6f174cf633bc0f508d1e
proxeeus <proxeeus@gmail.com>
2018-05-11 12:58:17 +0200

    cosmetic text fix

---
+++ paineel/library_assistant.lua
@@ -0,0 +1,5 @@
+function event_say(e)
+	if(e.message:findi("hail")) then
+		e.self:Say("Lots to do! Many books to place and scrolls to file! Feel free to browse but, please, don't make a mess!");
+	end
+end
