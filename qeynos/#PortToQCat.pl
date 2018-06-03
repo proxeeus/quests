1f82b615827396ef7a5113298813235777e72e5f
proxeeus <proxeeus@gmail.com>
2018-06-03 18:20:53 +0200

    qeynos to qcat dock tunnel zoning fixZ

--- qeynos/#PortToQCat.pl
+++ qeynos/#PortToQCat.pl
@@ -1,1 +1,1 @@
-sub EVENT_SPAWN{    $x = $npc->GetX();    $y = $npc->GetY();	quest::set_proximity($x - 15, $x + 15, $y - 25, $y + 25);} sub EVENT_ENTER{	quest::movepc(45,147,-175,-77,999);}
+sub EVENT_SPAWN{    $x = $npc->GetX();    $y = $npc->GetY();	$z = $npc->GetZ();	quest::set_proximity($x - 15, $x + 15, $y - 25, $y + 25, $z - 15, $z + 10);} sub EVENT_ENTER{	quest::movepc(45,147,-175,-77,999);}
