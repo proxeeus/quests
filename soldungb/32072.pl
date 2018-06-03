59b298609017937b00696d0bbd8c0ddd9fb4abbc
proxeeus <proxeeus@gmail.com>
2018-06-03 18:03:17 +0200

    soldungb to soldunga zonein adjustment

--- soldungb/32072.pl
+++ soldungb/32072.pl
@@ -4,9 +4,10 @@
 
 sub EVENT_SPAWN
 {
-	$x = $npc->GetX();
-	$y = $npc->GetY();
-	quest::set_proximity($x - 30, $x +30, $y - 30, $y + 30);
+	#Was30
+	#$x = $npc->GetX();
+	#$y = $npc->GetY();
+	quest::set_proximity($x - 15, $x +15, $y - 15, $y + 15);
 }
 
 sub EVENT_ENTER
