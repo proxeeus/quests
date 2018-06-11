43ec21b8456fe5e02779466421efbccb21291a41
proxeeus <proxeeus@gmail.com>
2018-06-11 13:26:40 +0200

    add Sontalak.lua

---
+++ westwastes/Sontalak.lua
@@ -0,0 +1,9 @@
+function event_say(e)
+	if(e.message:findi("hail")) then
+		e.self:Say(string.format("Why are you here %s? There is nothing you can gain from this land but an unsung death at the talons of your betters. If you want to prove your worth to Dragonkind, it will take more than slaying a few giants to accomplish it. Far more.",e.other:GetRace()));
+	elseif(e.message:findi("prove worth")) then
+		e.self:Say("Slaying all the Kromzek would be a good start. But that may be asking too much of such weak creatures.");
+	elseif(e.message:findi("the king is dead")) then
+		e.self:Say("Yes, morsel? You mean to tell me you have accomplished the task I set before you? Then it is proof I shall be having, or it is lunch I shall be having, do you understand?");
+	end
+end
