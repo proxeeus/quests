f0843e0b75e800e90159ddfa08170c1eaca8e014
proxeeus <proxeeus@gmail.com>
2018-07-04 13:36:39 +0200

    added fluff dialogue to Sir_Lindeal

---
+++ lavastorm/Sir_Lindeal.lua
@@ -0,0 +1,7 @@
+function event_combat(e)
+	e.self:Say("In the name of the Rainkeeper, I strike at thee!!");
+end
+
+function event_death_complete(e)
+	e.self:Say("Karana shall guide the Knights of Thunder to avenge the death of one of his flock.");
+end
