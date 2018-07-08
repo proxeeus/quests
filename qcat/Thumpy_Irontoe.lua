1db1719d198d58197388f46e618cc5f5fe4053cf
proxeeus <proxeeus@gmail.com>
2018-07-08 18:57:19 +0200

    added flavor for Thumpy Irontoe and the Shatblack brothers

---
+++ qcat/Thumpy_Irontoe.lua
@@ -0,0 +1,10 @@
+function event_combat(e)
+	if(e.joined) then
+		e.self:Say("Guards !! Guards !! Help me !");
+	end
+
+end
+
+function event_death_complete(e)
+	e.self:Say("My comrades will avenge my death.");
+end
