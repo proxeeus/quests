70508acd1672b475c4d5cc60ac0216d56f89f2ae
proxeeus <proxeeus@gmail.com>
2018-10-06 14:06:38 +0200

    added DT messages for greater sphinxes & hand of veeshan

---
+++ airplane/the_Hand_of_Veeshan.lua
@@ -0,0 +1,11 @@
+
+
+function event_cast_begin(e)
+	if(e.spell:ID() == 982) then
+		e.self:Shout(e.self:GetTarget():GetName() .. "!");
+	end
+end
+-------------------------------------------------------------------------------------------------
+-- Converted to .lua using MATLAB converter written by Stryd
+-- Find/replace data for .pl --> .lua conversions provided by Speedz, Stryd, Sorvani and Robregen
+-------------------------------------------------------------------------------------------------
