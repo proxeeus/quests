6740f565c35786735a5874865433c2d894239a92
proxeeus <proxeeus@gmail.com>
2019-02-28 17:02:43 +0100

    added all the remaining zones, maybe tweaks will be needed over time.

--- global/#Sky_Controller.pl
+++ global/#Sky_Controller.pl
@@ -3,22 +3,26 @@
 # This ensures that the day/night cycle is correctly visible. This is purely cosmetic
 # and has no bearing on conditional spawns etc!
 
-#burningwood mintime = 6 maxtime = 19
-#gfay 9 15
+# Dedicated Sky_Controller npc ID: 680
+
+# Zones to consider for UseMaxTime and general skybox use:
+# eastwastes, halas (no skybox is certain), misty, mistmoore, paineel, swampofnohope, tox, trakanon, wakening, warslikswood
 
 my		$morningSetup = "false";
 my		$nightSetup = "false";
 
 $mainTimer = 5;
-$subTimer = 5;
+$subTimer = 1;
 
 sub GetMinTime()
 {
-	if($zonesn eq "burningwood" || $zonesn eq "everfrost")
+	if($zonesn eq "burningwood" || $zonesn eq "everfrost" || $zonesn eq "eastwastes" || $zonesn eq "emeraldjungle" || $zonesn eq "feerrott" || $zonesn eq "halas"
+		|| $zonesn eq "innothule" || $zonesn eq "kithicor" || $zonesn eq "misty"|| $zonesn eq "nektulos"|| $zonesn eq "oggok"|| $zonesn eq "paineel"|| $zonesn eq "qrg"
+		|| $zonesn eq "swampofnohope"|| $zonesn eq "tox"|| $zonesn eq "trakanon"|| $zonesn eq "wakening"|| $zonesn eq "cabeast"|| $zonesn eq "cabwest")
 	{
 		return 6;
 	}
-	elsif($zonesn eq "gfaydark" || $zonesn eq "lfaydark")
+	elsif($zonesn eq "gfaydark" || $zonesn eq "lfaydark" || $zonesn eq "greatdivide"|| $zonesn eq "warslikswood")
 	{
 		return 9;
 	}
@@ -26,19 +30,29 @@ sub GetMinTime()
 
 sub GetMaxTime()
 {
-	if($zonesn eq "burningwood" || $zonesn eq "everfrost" )
+	if($zonesn eq "burningwood" || $zonesn eq "everfrost" || $zonesn eq "eastwastes" || $zonesn eq "emeraldjungle" || $zonesn eq "feerrott" || $zonesn eq "halas"
+		|| $zonesn eq "innothule"|| $zonesn eq "kithicor"|| $zonesn eq "misty"|| $zonesn eq "nektulos"|| $zonesn eq "oggok"|| $zonesn eq "paineel" || $zonesn eq "qrg"
+		|| $zonesn eq "swampofnohope"|| $zonesn eq "tox"|| $zonesn eq "trakanon"|| $zonesn eq "wakening"|| $zonesn eq "cabeast"|| $zonesn eq "cabwest")
 	{
 		return 20;
 	}
-	elsif($zonesn eq "gfaydark" || $zonesn eq "lfaydark")
+	elsif($zonesn eq "gfaydark" || $zonesn eq "lfaydark" || $zonesn eq "greatdivide")
 	{
 		return 15;
 	}
+	elsif($zonesn eq "warslikswood")
+	{
+		return 18;
+	}
 }
 
+# This helps the contoller determine if we should stick to a skyboxless night instead of a dark blueish one.
 sub UseMaxTime()
 {
-	if($zonesn eq "everfrost")
+	if($zonesn eq "everfrost" || $zonesn eq "burningwood" || $zonesn eq "emeraldjungle" || $zonesn eq "feerrott" || $zonesn eq "halas"
+		|| $zonesn eq "innothule"|| $zonesn eq "kithicor"|| $zonesn eq "misty"|| $zonesn eq "nektulos"|| $zonesn eq "oggok"|| $zonesn eq "paineel" || $zonesn eq "qrg"
+		|| $zonesn eq "swampofnohope"|| $zonesn eq "tox"|| $zonesn eq "trakanon"|| $zonesn eq "wakening"|| $zonesn eq "warslikswood"|| $zonesn eq "cabeast"
+		|| $zonesn eq "cabwest")
 	{
 		return "false";
 	}
@@ -50,6 +64,11 @@ sub UseMaxTime()
 
 sub EVENT_SPAWN
 {
+	$morningSetup = "false";
+	$nightSetup = "false";
+
+	$mainTimer = 5;
+	$subTimer = 5;
 	quest::settimer("sky", $mainTimer);
 }
 
