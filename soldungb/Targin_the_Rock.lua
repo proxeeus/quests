82393cee02107efddf26644ecd877187ee4d3270
proxeeus <proxeeus@gmail.com>
2019-12-28 20:18:22 +0100

    fix Targin the Rock dialogue

---
+++ soldungb/Targin_the_Rock.lua
@@ -0,0 +1,8 @@
+function event_combat(e)
+	if(e.joined) then
+		e.self:Say("You have much to learn.");
+		e.self:Say("Your actions and history are a personal affront to all I stand for.");
+	end
+end
+
+
