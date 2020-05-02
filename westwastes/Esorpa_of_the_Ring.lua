2c094b380d8e0ec052640fb1da1fc40f9889ff46
proxeeus <proxeeus@gmail.com>
2020-05-02 21:57:45 +0200

    add Esorpa_of_the_Ring.lua

---
+++ westwastes/Esorpa_of_the_Ring.lua
@@ -0,0 +1,5 @@
+function event_say(e)
+	if(e.message:findi("hail")) then
+		e.self:Say("You are truly a blight upon the planet. Here you are in this, the most holy of lands, and you DARE speak to me as if you were some sort of equal! We need to wipe the world clean, it is for the children of Veeshan, no others! I do not know what the Claws are thinking, our Mother would not approve of your kind, this much I know.");
+	end
+end
