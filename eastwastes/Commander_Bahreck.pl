332a02c0a37b059243b1d0e03f0ba10edc05908c
proxeeus <proxeeus@gmail.com>
2020-05-10 20:13:24 +0200

    Coldain ring quest fixes

--- eastwastes/Commander_Bahreck.pl
+++ eastwastes/Commander_Bahreck.pl
@@ -5,13 +5,13 @@ quest::spawn2(116553, 0,0, ($x + 100), ($y + 75), $z, $h);
 quest::spawn2(116553, 0,0, ($x + 150) , ($y - 15), $z, $h);
 quest::spawn2(116553, 0,0, ($x + 100) , ($y - 75), $z, $h);
 quest::spawn2(116553, 0,0, ($x - 10), $y, $z, $h);
-#quest::spawn2(KROMRIFF SOLIDER ID, 0,0, -2089, 98, 147, 58);
-#quest::spawn2(KROMRIFF SOLIDER ID, 0,0, -2112, 98, 147, 58);
+quest::spawn2(116548, 0,0, -2089, 98, 147, 58);
+quest::spawn2(116548, 0,0, -2112, 98, 147, 58);
 
 }
 
 sub EVENT_DEATH_COMPLETE {
 
-quest::signal(116119, 0);
+quest::signal(116574, 0);
 
 }
--- eastwastes/Dobbin_Crossaxe.pl
+++ eastwastes/Dobbin_Crossaxe.pl
@@ -1,33 +1,27 @@
+my $ring_seven_final = undef;
+
 sub EVENT_SIGNAL {
-
-	if($signal == 1)
-	{
-	$ring_seven_final=1;
-	quest::say("We thought it was too late, the Dain will be very pleased!");
-	quest::say("Please friend, show me your Mithril ring and I will show you our gratitude.");
-	}
-
-	if($signal == 2)
-	{
-	$ring_seven_final=undef;
-	}
-
+  if($signal == 1){
+    $ring_seven_final = 1;
+    quest::say("We thought it was too late, the Dain will be very pleased!");
+    quest::say("Please friend, show me your Mithril ring and I will show you our gratitude.");
+  }
 }
 
 sub EVENT_ITEM { 
-		if($itemcount{30162} == 1 && $ring_seven_final == 1){
+  if($ring_seven_final == 1 && plugin::check_handin(\%itemcount, 30162 => 1)){
+    quest::say("Thank you. $name. your deeds will be mentioned to the Dain. Please take this note from Corbin to Garadain. it may help him to achieve victory over the enemy.");
+    quest::summonitem(1047); # Item: Note from Corbin
+    quest::faction(406,5); # Faction: Coldain
+    quest::faction(405,5); # Faction: Dain Frostreaver IV
+    quest::faction(419,-10); # Faction: Kromrif
+    quest::faction(448,-10); # Faction: Kromzek
+	quest::exp(100000);
+	quest::ding();
+    $ring_seven_final=undef;
+  }
 
-			quest::say("Thank you. $name. your deeds will be mentioned to  the Dain. Please take this note from Corbin to Garadain. it may help him to achieve victory over the enemy.");
-			$ring_seven_final=undef;
-			quest::summonitem(1047);
-			quest::faction(406,5);
-			quest::faction(405,5);
-			quest::faction(419,-10);
-			quest::faction(448,-10);
-		 }
-
-
+  plugin::return_items(\%itemcount);
 }
 
-#END of FILE Zone:eastwastes  ID:116129 -- Dobbin_Crossaxe 
-
+#END of FILE Zone:eastwastes  ID:116129 -- Dobbin_Crossaxe
