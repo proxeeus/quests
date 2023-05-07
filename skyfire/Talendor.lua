4791fd3bbb6f62a6d9ab707d6263976b893eaaf2
proxeeus <proxeeus@gmail.com>
2023-05-07 16:25:45 +0200

    Talendor flavor text

--- skyfire/Talendor.lua
+++ skyfire/Talendor.lua
@@ -1,3 +1,11 @@
 function event_slay(e)
 	e.self:Say("Slightly crispy but still tasty, who shall be next?");
 end
+
+function event_combat(e)
+	if(e.joined == true) then
+	  e.self:Shout("How amusing. You foolish mortals actually think to challenge me? Very well, then, I shall enjoy searing the flesh from your bones!");
+	end
+  end
+  
+  
