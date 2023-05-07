a19f237ebb1a2fee4cb38323fcc69925f9a24bd2
proxeeus <proxeeus@gmail.com>
2023-05-07 16:38:08 +0200

    runnyeye flavor texts

---
+++ runnyeye/#Battlelord_Paluk.lua
@@ -0,0 +1,9 @@
+function event_combat(e)
+	e.self:Say("Invade our home, kill our young?  It is on a spike your head will be hung!");
+end
+
+
+function event_death(e)
+	e.self:Say("I tried my best, yet now I am dead.  It looks as if you will keep your head.");
+end
+
