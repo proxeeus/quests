20532b86e4522e13bcf7fd99511f938e8a094a3d
proxeeus <proxeeus@gmail.com>
2024-01-20 19:38:04 +0100

    sleeper flavor

--- sleeper/A_warning.lua
+++ sleeper/A_warning.lua
@@ -1,4 +1,10 @@
-function event_combat(e)
-	e.self:Emote("fills your mind ``HEED THESE WORDS!  This prison was made to hold the most dangerous being ever to walk the face of Norrath.  The combined might of all Wurmkind was required to subdue The Sleeper, and only eternal vigilance will keep him imprisoned.  If you are not here on behest of the Council, you will be destroyed unless you depart now.  YOU HAVE BEEN WARNED!``");
+function event_enter(e)
+	e.self:Emote("fills your mind `HEED THESE WORDS!  This prison was made to hold the most dangerous being ever to walk the face of Norrath.  The combined might of all Wurmkind was required to subdue The Sleeper, and only eternal vigilance will keep him imprisoned.  If you are not here on behest of the Council, you will be destroyed unless you depart now.  YOU HAVE BEEN WARNED!`");
 	eq.depop_with_timer();
+end
+
+function event_spawn(e)
+	local xloc = e.self:GetX();
+	local yloc = e.self:GetY();
+	eq.set_proximity(xloc - 50, xloc + 50, yloc - 50, yloc + 50);
 end
---
+++ sleeper/Master_of_the_Guard.lua
@@ -0,0 +1,3 @@
+function event_combat(e)
+	e.self:Shout("You shall not pass this point.");
+end
