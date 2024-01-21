3cf96fb991a12c8a5c5020797a599626b80680ce
proxeeus <proxeeus@gmail.com>
2024-01-21 10:02:19 +0100

    add missing text to sleeper/Master_of_the_Guard.lua

--- sleeper/Master_of_the_Guard.lua
+++ sleeper/Master_of_the_Guard.lua
@@ -1,3 +1,7 @@
 function event_combat(e)
 	e.self:Shout("You shall not pass this point.");
 end
+
+function event_death_complete(e)
+	e.self:Shout("Foolish mortals, this one is but one of many, you will not survive the rest!!")
+end
