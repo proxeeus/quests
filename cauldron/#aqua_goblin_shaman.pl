a993aba1bf3f27a1d588a154d7999f0f15efdf99
proxeeus <proxeeus@gmail.com>
2024-08-04 16:25:49 +0200

    tweak bilge farfathom cycle

--- cauldron/#aqua_goblin_shaman.pl
+++ cauldron/#aqua_goblin_shaman.pl
@@ -6,12 +6,9 @@ sub EVENT_DEATH{
  my $random_result = int(rand(100));
  my $a=70059; #Bilge
  my $b=70063; #npc - #a_cauldron_rat
- my $c=70069; #The_Fabled_Bilge_Farfathom
  if($random_result<85){
   quest::spawn2($b,6,0,-974.5,436.4,-343.8,58);
   }elsif($random_result<95){
   quest::spawn2($a,6,0,-974.5,436.4,-343.8,58);
-  }else{
-    quest::spawn2($c,6,0,-974.5,436.4,-343.8,58);
-  };
+  }
  };
